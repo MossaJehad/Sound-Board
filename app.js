a33f4a8e03108c1519facada18541fae1d7fef89
MossaJehad <Moosa.Jehad65@gmail.com>
2023-02-24 23:59:44 +0300

    #

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const sounds = ['abushawqi', 'alasfor', 'dinoandfrog', 'skeleton']
 
 sounds.forEach(sound => {
     const btn = document.createElement('button')
-    btn.classList.add('button', 'is-dark', 'm-2')
+    btn.classList.add('button', 'is-dark', 'm-2', 'p-5')
+
+    btn.addEventListener('click', () => {
+        document.getElementById(sound).play()
+    })
 
     btn.innerText = sound
     document.getElementById('buttons').appendChild(btn)
