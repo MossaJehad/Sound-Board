212258e971be2ca51956ac5920992018f466dbcd
MossaJehad <Moosa.Jehad65@gmail.com>
2023-02-25 00:04:21 +0300

    #

--- app.js
+++ app.js
@@ -5,9 +5,20 @@ sounds.forEach(sound => {
     btn.classList.add('button', 'is-dark', 'm-2', 'p-5')
 
     btn.addEventListener('click', () => {
+        stopSongs()
         document.getElementById(sound).play()
     })
 
     btn.innerText = sound
     document.getElementById('buttons').appendChild(btn)
 })
+
+
+function stopSongs(){
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
